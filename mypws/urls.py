6389baeaa6b0ecb7ff53f26e56fac22a02608071
christine <binbinnaska@gmail.com>
2018-03-11 22:27:29 -0400

    include modified files

--- mypws/urls.py
+++ mypws/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^release_manifest/', include('release_manifest.urls')),
+#    url(r'^release_manifest/', include('release_manifest.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls'))
 ]
